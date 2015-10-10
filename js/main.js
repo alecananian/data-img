4d6494ec93b0477916c41d18089ad43f5cd17f48
Alec Ananian <alec.ananian@warnerbros.com>
2015-10-10 13:19:15 -0700

    Added browser compatibility check

--- js/main.js
+++ js/main.js
@@ -1,28 +1,36 @@
 $(function () {
-  var $encoder = $('#encoder');
-  $('input', $encoder).change(function () {
-    var file = this.files[0];
-    if (file) {
-      var reader = new FileReader();
-      
-      reader.onloadend = function () {
-        $data_uri.val(reader.result).trigger('change');
-      }
-      
-      reader.readAsDataURL(file);
-    }
-  });
+  var $app_container = $('#app-container');
+  var $app_error = $('#app-error');
   
-  var $decoder = $('#decoder');
-  var $image_preview = $('#image-preview');
-  var $image = $('img', $image_preview);
-  var $data_uri = $('textarea', $decoder).on('change keyup cut paste', function () {
-    var data_uri = $(this).val(); 
-    if (data_uri && data_uri.length) {
-      $image_preview.show();
-      $image.attr('src', data_uri);
-    } else {
-      $image_preview.hide();
-    }
-  });
+  if (window.FileReader) {
+    var $encoder = $('#encoder');
+    $('input', $encoder).change(function () {
+      var file = this.files[0];
+      if (file) {
+        var reader = new FileReader();
+        
+        reader.onloadend = function () {
+          $data_uri.val(reader.result).trigger('change');
+        }
+        
+        reader.readAsDataURL(file);
+      }
+    });
+    
+    var $decoder = $('#decoder');
+    var $image_preview = $('#image-preview');
+    var $image = $('img', $image_preview);
+    var $data_uri = $('textarea', $decoder).on('change keyup cut paste', function () {
+      var data_uri = $(this).val(); 
+      if (data_uri && data_uri.length) {
+        $image_preview.show();
+        $image.attr('src', data_uri);
+      } else {
+        $image_preview.hide();
+      }
+    });
+  } else {
+    $app_container.hide();
+    $app_error.html('Sorry, your browser is not compatible with this app. Please upgrade to a <a href="http://caniuse.com/#feat=filereader" target="_blank">compatible browser</a>.').removeClass('collapse');
+  }
 }());
